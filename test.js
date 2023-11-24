9b8c31fe11099c6ab1b235bac73366e96191f6f3
ShlokMathur <80643183+ShlokMathur@users.noreply.github.com>
2023-11-24 13:05:24 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("Shlok Mathur")
+const accountId = 11057
+let emailId = "shlok.mathur2001@gmail.com"
+var password = "23456"
+
+
+emailId = "shlok.mathur2020@vitbhopal.ac.in"
+password = "4563733"
+
+console.table([accountId, emailId, password])
---
+++ variables_and_constant.js
@@ -0,0 +1,12 @@
+const accountId = 11057
+let emailId = "shlok.mathur2001@gmail.com"
+var password = "23456"
+
+
+//  accountId = 10643 cannot be used as const can't be changed once declared
+
+
+emailId = "shlok.mathur2020@vitbhopal.ac.in"
+password = "4563733"
+
+console.table([accountId, emailId, password])
